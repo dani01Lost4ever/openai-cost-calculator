bc8568f87d02f98f3f38970e494f09aadcb862a7
daniel busetto <daniel.busetto@venicecom.it>
2024-08-19 12:09:48 +0200

    .d.ts declaration

---
+++ src/index.d.ts
@@ -0,0 +1,32 @@
+﻿declare module 'openai-token-cost-calculator-updated' {
+
+    interface LanguageModelCostOptions {
+        promptTokens?: number;
+        completionTokens?: number;
+    }
+
+    interface CostResult {
+        promptCost?: number;
+        completionCost?: number;
+        totalCost: number;
+        formattedTotalCost: string;
+    }
+
+    function calculateLanguageModelCost(modelName: string, options: LanguageModelCostOptions): CostResult;
+
+    function calculateFineTuningModelCost(modelName: string, tokens: number, operation: string): CostResult;
+
+    function calculateEmbeddingModelCost(modelName: string, tokens: number): CostResult;
+
+    function calculateImageModelCost(modelName: string, quality: string, resolution: string, images: number): CostResult;
+
+    function calculateAudioModelCost(modelName: string, minutes: number): CostResult;
+
+    export {
+        calculateLanguageModelCost,
+        calculateFineTuningModelCost,
+        calculateEmbeddingModelCost,
+        calculateImageModelCost,
+        calculateAudioModelCost
+    };
+}
